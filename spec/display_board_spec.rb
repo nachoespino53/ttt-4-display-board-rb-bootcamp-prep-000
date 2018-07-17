2536e02042e5760ce5dceb92952d6f8d7aa6d7cb
Ignacio R Espino <irespino@capozios.com>
2018-07-17 16:36:12 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -177,7 +177,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
     it 'prints an entire board full of Os' do
 
-    board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+    board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
     output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
      expect(rows[0]).to eq(" O | O | O ")
